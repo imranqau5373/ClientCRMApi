518fe7b38fcb3bd33537e599fd8cd3c908aa56a3
Imran Khan <Imran.khan@synergy.it>
2019-02-18 00:31:37 +0500

    log file changes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const opts = {
+  errorEventName:'error',
+      logDirectory:'', // NOTE: folder must exist and be writable...
+      fileNamePattern:'',
+      dateFormat:'YYYY.MM.DD'
+};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,8 +15,24 @@ router.get('/', function(req, res, next) {
 
 router.post('/clientcrm', function(req, res, next) {
   let apiData = {}
-  apiData.lastName = req.body.lastName;
+  console.log('in client crm')
+  opts.logDirectory = './public/logFiles/';
+  opts.fileNamePattern = 'request.txt';
+  const log = require('simple-node-logger').createRollingFileLogger( opts );
   
-  res.json(req.body)
+
+  apiData.lastName = req.body.lastName;
+  log.info('subscription to ',apiData, ' accepted at ', new Date().toJSON());
+  console.log(apiData)
+  res.json(req.body);
+  // fs.writeFile("/logFiles/request.txt", apiData, function(err) {
+  //     if(err) {
+  //         return console.log(err);
+  //     }
+
+  //     console.log("The file was saved!");
+  //     res.json(req.body);
+  // });
+
 });
 module.exports = router;
