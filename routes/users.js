ba8495876fdc8a6b429d307d8c76c3fb0965ead6
Imran Khan <Imran.khan@synergy.it>
2019-02-17 20:00:43 +0500

    My new change

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.post('/clientcrm', function(req, res, next) {
+  let apiData = {}
+  apiData.lastName = req.body.lastName;
+  
+  res.json(req.body)
+});
 module.exports = router;
