28980e3bbc1eb23cd3f5e83eb46c915fcd0ea3ff
Imran Khan <Imran.khan@synergy.it>
2019-05-12 18:19:14 +0500

    new client changes

--- routes/users.js
+++ routes/users.js
@@ -45,4 +45,43 @@ request(options, function (error, response,crmData) {
 
 
 });
+
+
+
+router.post('/affliates', function(req, res, next) {
+  let apiData = {}
+    console.log('in client crm body',req.body)
+
+  opts.logDirectory = './public/logFiles/';
+  opts.fileNamePattern = 'request.txt';
+  // const log = require('simple-node-logger').createRollingFileLogger( opts );
+
+  apiData.lastName = req.body.lastName;
+  apiData.firstName = req.body.firstName;
+  apiData.email = req.body.email;
+  apiData.phone = req.body.phone;
+  apiData.countryCode = req.body.countryCode;
+  apiData.password  = req.body.password;
+  console.log(apiData);
+  var options = { method: 'POST',
+  url: 'https://api.water-pipeline.com/SignalsServer/api/registerUser',
+  qs: 
+   { firstname: apiData.firstName,
+     lastname: apiData.lastName,
+     email: apiData.email,
+     telephone: apiData.phone,
+     countryiso: apiData.countryCode,
+     apikey: '51363aa0ca7743f2af06a45308e87baa' },
+  headers: 
+   { 'Postman-Token': '62b16b75-d54d-48bf-865c-7d93f3c7539d',
+     'cache-control': 'no-cache' } };
+
+request(options, function (error, response, body) {
+  if (error)
+    res.json(error);
+    else
+    res.json(body);
+});
+
+});
 module.exports = router;
